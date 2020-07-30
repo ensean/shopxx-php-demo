a1f3b00effbb870cdbfd7f718e52c31bfc3d1b1e
liyxzsu <liyxzsu@gmail.com>
2020-07-30 15:39:44 +0800

    fix typo

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+                <p>The server is running PHP version <?php echo phpversion(); ?>.</p>
             </div>
         </div>
 
